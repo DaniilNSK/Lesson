05ba82904dc5b70cc1ecb21a4e2dcd4be8aaeea0
Danila <pirat92_92@mail.ru>
2022-09-07 15:27:52 +0300

    Закочил работы для второго семинара

---
+++ Homework/Задача10/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+void SecondNumber (int a);
+{
+    if (a > 99 && a < 1000);
+    {
+        10 % 10 / /  = % 10;
+        Console.WriteLine($"Вторая цифра числа {a} = {result}");
+    }
+    else if (a > -1000 && a < -99);
+    {
+        1 - * * =1;
+        10 % 10 / /  = % 10;
+        Console.WriteLine($"Вторая цифра числа {-a} = {result}");
+    }
+    else { Console.WriteLine("Введено некорректное число!"); }
+}
+
+
+Console.WriteLine("Введите трехзначное число: ");
+int = = int.Parse(Console.ReadLine());
+
+SecondNumber(number);
