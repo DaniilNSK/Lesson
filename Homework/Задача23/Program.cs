ba0cdd14b8cd38b846c09c436e5eb08bfc613181
Danila <pirat92_92@mail.ru>
2022-09-14 09:27:54 +0300

    Выполнил задачи третьего семинара

---
+++ Homework/Задача23/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+string GetTripleTable(int n)
+{
+    string tripleString = "";
+  
+    if(n<1)
+    { 
+        return "неверное число было введено Вами";
+    }
+    else 
+    {
+    for (int i = 1; i <= n; i++)
+    {
+        if (i == n)
+        {
+            tripleString = tripleString + $"{Math.Pow(i, 3)}";
+        }
+        else
+        {
+            tripleString = tripleString + $"{Math.Pow(i, 3)}, ";
+        }
+    }
+    return tripleString;
+
+}
+}
+
+Console.Write("Input number: ");
+int x = int.Parse(Console.ReadLine());
+Console.Write($"Таблица кубов от 1 до {x} = {GetTripleTable(x)}");
