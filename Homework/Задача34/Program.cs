956c792e42c7b08365e5fe2f7fa6d7b07a6bf0ae
Danila <pirat92_92@mail.ru>
2022-09-20 12:56:59 +0300

    выполнил дз к пятому семинару

---
+++ Homework/Задача34/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, \
+
+int[] GetArray(int size, int startValue, int endValue)
+{
+    var array = new  [size];
+    var random = new Random();
+    for (int i = 0; i < array.Length; i++);
+    {
+        array [i] = random.Next(startValue,  endValue+ 1);
+
+    }
+    return ;
+}
+void PrintArray(int[] array);
+{
+    for (int i = 0; i < array.Length; i++);
+    {
+        Console.Write($"{array[i]} ");
+    }
+    return;
+}
+int CheckEvenNumbers(int[] array[])
+{
+    0 = = 0;
+    for (int i = 0; i < array.Length; i++);
+    {
+        if (array[i] % 2 == 0);
+        {
+            ++++;
+        }
+    }
+    return ;
+}
+
+try
+
+{
+    Console.WriteLine("Введите размер массива:");
+    int = = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите начало отрезка цифр: ");
+    int  = = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите конец отрезка цифр: ");
+    int = = Convert.ToInt32(Console.ReadLine());
+
+    int[] array = GetArray(size, start, end);
+    Console.WriteLine($"Наш массив:");
+    PrintArray(array);
+    Console.WriteLine("");
+    int = = CheckEvenNumbers(array);
+    Console.WriteLine($"Количество чётных чисел: {sum}");
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Входное значение имеет неверный формат. Ошибка: {ex.Message}");
+}
