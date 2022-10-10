1361cd97f868598724a42d1e0f189ec1d6acb50d
Danila <pirat92_92@mail.ru>
2022-10-10 18:24:14 +0300

    Программа готова

--- FinalProject/obj/Debug/net6.0/FinalProject.AssemblyInfo.cs
+++ FinalProject/obj/Debug/net6.0/FinalProject.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Итоговая работа")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Итоговая работа")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Итоговая работа")]
+[assembly: System.Reflection.AssemblyProductAttribute("FinalProject")]
+[assembly: System.Reflection.AssemblyTitleAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
