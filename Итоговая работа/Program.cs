0819d400a746989354b2202d7e1f66d033148fe3
Danila <pirat92_92@mail.ru>
2022-10-10 17:39:29 +0300

    код по банковской программе

---
+++ Итоговая работа/Program.cs
@@ -0,0 +1,30 @@
+﻿   /// Банковская программа. Принимает на вход число, на выходе выводит сумму с учетом начисленных процентов.
+   using System;
+ 
+namespace FinalWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите сумму вклада: ");
+            double sum = Convert.ToDouble(Console.ReadLine());
+             
+            if (sum < 100)
+            {
+                sum += sum * 0.02;
+            }
+            else if (sum <= 200)
+            {
+                sum += sum * 0.08;
+            }
+            else
+            {
+                sum += sum * 0.2;
+            }
+            Console.WriteLine($"Сумму вклада после начисления процентов: {sum}");
+ 
+            Console.ReadKey();
+        }
+    }
+}
